b88afcbb622baa5afd8916afada7795d5e57fc3f
tanvohung <tanvohung@192.168.7.111>
2019-04-28 00:16:16 +0700

    Finish TC_01 and TC_02

--- src/selenium/Topic_02_Xpath_Css_Part_I.java
+++ src/selenium/Topic_02_Xpath_Css_Part_I.java
@@ -6,6 +6,7 @@ import org.testng.annotations.BeforeClass;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
@@ -23,28 +24,21 @@ public class Topic_02_Xpath_Css_Part_I {
   }
   
   @Test
-  public void TC_01_CheckUrl() {
-	  String homePageUrl = driver.getCurrentUrl();
-	  Assert.assertEquals("http://live.guru99.com/", homePageUrl);
+  public void TC_01_LoginEmpty() {
+	  driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+	  driver.findElement(By.id("send2")).click();
+	  Assert.assertEquals("This is a required field.", driver.findElement(By.id("advice-required-entry-email")).getText());
+	  Assert.assertEquals("This is a required field.", driver.findElement(By.id("advice-required-entry-pass")).getText());
   }
   
   @Test
-  public void TC_02_CheckTitle() {
-	  String homePageTitle = driver.getTitle();
-	  Assert.assertEquals("Home page", homePageTitle);
+  public void TC_02_LoginWithEmailInvalid() {
+	  driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+	  driver.findElement(By.id("email")).sendKeys("1234534313@123412.123412");
+	  driver.findElement(By.id("send2")).click();
+	  Assert.assertEquals("Please enter a valid email address. For example johndoe@domain.com.", driver.findElement(By.id("advice-validate-email-email")).getText());
   }
   
-  @Test
-  public void TC_03_CheckPanel() {
-	  String homePageTitle = driver.getTitle();
-	  Assert.assertEquals("Home page", homePageTitle);
-  }
-  
-  @Test
-  public void TC_04_CheckPanel() {
-	  String homePageTitle = driver.getTitle();
-	  Assert.assertEquals("Home page", homePageTitle);
-  }
   
   @AfterClass
   public void afterClass() {
