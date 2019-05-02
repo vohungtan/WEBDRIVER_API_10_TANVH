2f5b35c36dc03624fe955363de56ac82176c0408
tanvohung <tanvohung@192.168.7.111>
2019-05-02 22:23:44 +0700

    Finish TC_04

--- src/selenium/Topic_02_Xpath_Css_Part_I.java
+++ src/selenium/Topic_02_Xpath_Css_Part_I.java
@@ -39,6 +39,23 @@ public class Topic_02_Xpath_Css_Part_I {
 	  Assert.assertEquals("Please enter a valid email address. For example johndoe@domain.com.", driver.findElement(By.id("advice-validate-email-email")).getText());
   }
   
+  @Test
+  public void TC_03_LoginWithPasswordLessThan6Characters() {
+	  driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+	  driver.findElement(By.id("email")).sendKeys("automation@gmail.com");
+	  driver.findElement(By.id("pass")).sendKeys("123");
+	  driver.findElement(By.id("send2")).click();
+	  Assert.assertEquals("Please enter 6 or more characters without leading or trailing spaces.", driver.findElement(By.id("advice-validate-password-pass")).getText());
+  }
+  
+  @Test
+  public void TC_04_LoginWithPasswordIncorrect() {
+	  driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+	  driver.findElement(By.id("email")).sendKeys("automation@gmail.com");
+	  driver.findElement(By.id("pass")).sendKeys("123123123");
+	  driver.findElement(By.id("send2")).click();
+	  Assert.assertEquals("Invalid login or password.", driver.findElement(By.xpath("//span[text()='Invalid login or password.']")).getText());
+  }
   
   @AfterClass
   public void afterClass() {
