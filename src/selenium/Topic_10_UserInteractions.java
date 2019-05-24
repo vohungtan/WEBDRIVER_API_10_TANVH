ed1213e732b6f99f2c14c145c3e1fa1e1f5fcd9e
tanvohung <tanvohung@192.168.7.111>
2019-05-24 21:36:30 +0700

    Finish_TC04,05,06_Topic10

--- src/selenium/Topic_10_UserInteractions.java
+++ src/selenium/Topic_10_UserInteractions.java
@@ -7,7 +7,9 @@ import org.testng.annotations.BeforeClass;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -17,7 +19,7 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 
 public class Topic_10_UserInteractions {
-	
+    
   WebDriver driver;
   
   @BeforeClass
@@ -26,7 +28,7 @@ public class Topic_10_UserInteractions {
 	  driver.manage().window().maximize();
   }
   
-  
+  @Test
   public void TC_01_MoveMouseToElement() throws Exception {
 	  driver.get("http://www.myntra.com/");
 	  
@@ -49,8 +51,7 @@ public class Topic_10_UserInteractions {
 	  
   }
   
-  
-  
+  @Test
   public void TC_02_ClickAndHoldElement() throws Exception {
 	  driver.get("http://jqueryui.com/resources/demos/selectable/display-grid.html");
 	  
@@ -96,9 +97,73 @@ public class Topic_10_UserInteractions {
 	  Assert.assertEquals(numberSelectedItem.size(), 4);
   }
   
+  @Test
+  public void TC_04_DoubleClick() throws Exception {
+	  driver.get("http://www.seleniumlearn.com/double-click");
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  
+	  WebElement doubleClickMe = driver.findElement(By.xpath("//button[text()='Double-Click Me!']"));
+	  
+	  ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", doubleClickMe);
+	  
+	  Actions action = new Actions(driver);
+	  
+	  action.doubleClick(doubleClickMe).perform();
+	  Thread.sleep(2000);
+	  
+	  Alert alert = driver.switchTo().alert();
+	  
+	  Assert.assertEquals(alert.getText(), "The Button was double-clicked.");
+	  
+	  alert.accept();
+	  
+  }
+  
+  @Test
+  public void TC_05_RightClick() throws Exception {
+	  driver.get("http://swisnl.github.io/jQuery-contextMenu/demo.html");
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  
+	  WebElement rightClickMe = driver.findElement(By.xpath("//span[text()='right click me']"));
+	  
+	  Actions action = new Actions(driver);
+	  action.contextClick(rightClickMe);
+	  
+	  WebElement quitButton = driver.findElement(By.xpath("//li[contains(@class,'context-menu-icon-quit')]/span[text()='Quit']"));
+	  action.moveToElement(quitButton);
+	  
+	  Assert.assertTrue(driver.findElement(By.xpath("//li[contains(@class,'context-menu-visible') and contains(@class,'context-menu-hover')]/span[text()='Quit'])")).isDisplayed());
+	  
+	  quitButton.click();
+	  
+	  Alert alert = driver.switchTo().alert();
+	  
+	  alert.accept();
+	  
+  }
+  
+  
+  @Test
+  public void TC_06_DragAndDrop() throws Exception {
+	  driver.get("http://demos.telerik.com/kendo-ui/dragdrop/angular");
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  
+	  WebElement sourceItem = driver.findElement(By.xpath("//div[@id='draggable']"));
+	  
+	  WebElement targetItem = driver.findElement(By.xpath("//div[@id='droptarget']"));
+	  
+	  Actions action = new Actions(driver);
+	  
+	  action.dragAndDrop(sourceItem, targetItem).build().perform();
+	  Thread.sleep(2000);
+	  
+	  Assert.assertEquals(driver.findElement(By.xpath("//div[text()='You did great!']")).getText(), "You did great!");
+	  
+  }
+  
   @AfterClass
   public void afterClass() {
-	  //driver.quit();
+	  driver.quit();
   }
 
 }
